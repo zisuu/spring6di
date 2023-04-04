381a0006973eb1d2de44dffb828e21ad9cda6554
zisuu <david.ziswiler@finecloud.ch>
2023-04-04 15:46:42 +0200

    bean life cycle

--- src/main/java/ch/finecloud/spring6di/controllers/MyController.java
+++ src/main/java/ch/finecloud/spring6di/controllers/MyController.java
@@ -17,4 +17,12 @@ public class MyController {
         System.out.println("I'm the controller");
         return greetingService.sayGreeting();
     }
+
+    public void beforeInit() {
+        System.out.println("## - Before Init - Called by Bean Post Processor");
+    }
+
+    public void afterInit() {
+        System.out.println("## - After init called by Bean Post Processor");
+    }
 }
