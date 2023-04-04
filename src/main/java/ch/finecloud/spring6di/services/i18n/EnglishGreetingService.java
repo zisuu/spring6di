533aa16fabc6cb0b572b1b5291a286852fcc9946
zisuu <david.ziswiler@finecloud.ch>
2023-04-04 14:20:11 +0200

    default profile

--- src/main/java/ch/finecloud/spring6di/services/i18n/EnglishGreetingService.java
+++ src/main/java/ch/finecloud/spring6di/services/i18n/EnglishGreetingService.java
@@ -4,7 +4,7 @@ import ch.finecloud.spring6di.services.GreetingService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN", "default"})
 @Service("i18nService")
 public class EnglishGreetingService implements GreetingService {
     @Override
--- src/test/java/ch/finecloud/spring6di/controllers/i18n/myi18nControllerTestES.java
+++ src/test/java/ch/finecloud/spring6di/controllers/i18n/myi18nControllerTestES.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("ES")
+//@ActiveProfiles("ES")
 @SpringBootTest
 public class myi18nControllerTestES {
 
